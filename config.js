b0fc8a6f80f7592adf15dea8b112abea81807418
Qian Zhang <qianzhang019@gmail.com>
2023-03-20 12:44:06 +0000

    update

---
+++ config.js
@@ -0,0 +1,7 @@
+export function configF() {
+    const apiKey = 'ccb28ddf-abfc-4af7-bce6-0c4d6270bdb9';
+    const searchUrl = `https://content.guardianapis.com/search?q=weather&show-fields=thumbnail,trailText&api-key=${apiKey}`;
+    return searchUrl;
+}
+
+
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const searchUrl = "https://content.guardianapis.com/search?q=weather&show-fields=thumbnail,trailText&api-key=ccb28ddf-abfc-4af7-bce6-0c4d6270bdb9";
+// import { config } from "dotenv";
+// config()
+
+// const apiKey = process.env.API_KEY;
+import fetch from 'node-fetch';
+import { configF } from './config.js';
+const searchUrl = configF();
+
+//const searchUrl = `https://content.guardianapis.com/search?q=weather&show-fields=thumbnail,trailText&api-key=ccb28ddf-abfc-4af7-bce6-0c4d6270bdb9`;
 
 document.addEventListener("DOMContentLoaded", function () {
 
